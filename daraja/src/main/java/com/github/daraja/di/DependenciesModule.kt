bd926a35d6cd304af53f93d2e44cf43f30486ca9
karis <eric.kariuki@dlight.com>
2022-10-31 09:40:53 +0300

    DARAJA: Add Create an ssl socket factory with our all-trusting manager

--- daraja/src/main/java/com/github/daraja/di/DependenciesModule.kt
+++ daraja/src/main/java/com/github/daraja/di/DependenciesModule.kt
@@ -15,6 +15,7 @@
  */
 package com.github.daraja.di
 
+import android.annotation.SuppressLint
 import com.github.daraja.BuildConfig
 import com.github.daraja.services.STKPushService
 import com.github.daraja.utils.Constants
@@ -22,7 +23,12 @@ import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.security.SecureRandom
+import java.security.cert.X509Certificate
 import java.util.concurrent.TimeUnit
+import javax.net.ssl.SSLContext
+import javax.net.ssl.TrustManager
+import javax.net.ssl.X509TrustManager
 
 object DependenciesModule {
 
@@ -39,6 +45,29 @@ object DependenciesModule {
             .writeTimeout(Constants.WRITE_TIMEOUT.toLong(), TimeUnit.SECONDS)
             .readTimeout(Constants.READ_TIMEOUT.toLong(), TimeUnit.SECONDS)
             .addInterceptor(httpLoggingInterceptor)
+
+        try {
+            val trustAllCerts = arrayOf<TrustManager>(
+                @SuppressLint("TrustAllX509TrustManager")
+                object : X509TrustManager {
+                    override fun checkClientTrusted(chain: Array<X509Certificate>, authType: String) {}
+                    override fun checkServerTrusted(chain: Array<X509Certificate>, authType: String) {}
+                    override fun getAcceptedIssuers(): Array<X509Certificate> = arrayOf()
+                }
+            )
+
+            // Install the all-trusting trust manager
+            val sslContext = SSLContext.getInstance("SSL")
+            sslContext.init(null, trustAllCerts, SecureRandom())
+
+            // Create an ssl socket factory with our all-trusting manager
+            val sslSocketFactory = sslContext.socketFactory
+            builder.sslSocketFactory(sslSocketFactory, (trustAllCerts[0] as X509TrustManager))
+            builder.hostnameVerifier { _, _ -> true }
+        } catch (e: Exception) {
+            throw RuntimeException(e)
+        }
+
         return builder.build()
     }
 
